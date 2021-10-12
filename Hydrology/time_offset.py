18d228fd06464c88dd7b9a0d414bad673524fa84
Nick James <nicholas.james@sydney.edu.au>
2021-10-13 10:07:53 +1100

    Update for Dynamic Time Warping

--- Hydrology/time_offset.py
+++ Hydrology/time_offset.py
@@ -6,7 +6,8 @@ import glob
 from scipy.signal import welch
 import datetime
 from Utils import dendrogram_plot_labels
-from matplotlib import colors
+from scipy.spatial.distance import euclidean
+from fastdtw import fastdtw
 
 make_plots = False
 
@@ -70,52 +71,78 @@ max_offset = 180
 # Set offset matrix
 offset_matrix = np.zeros((len(time_series_list),len(time_series_list)))
 l1_matrix = np.zeros((len(time_series_list),len(time_series_list)))
+dtw_matrix = np.zeros((len(time_series_list),len(time_series_list)))
 
 # Time series i and j
 for i in range(len(time_series_list)):
-    for j in range(len(time_series_list)): #len(time_series_list)
+    for j in range(len(time_series_list)):
+        distance, path = fastdtw(time_series_list[i], time_series_list[j], radius = 180, dist=euclidean)
+        dtw_matrix[i,j] = distance
+        print("Distance", distance)
+    print("Iteration", i)
 
-        # l1 lag
-        l1_scores_i = []
-        for k in range(0,max_offset,1):
-            # time series i and j
-            if k == 0:
-                flow_i = time_series_list[i]
-                flow_j = time_series_list[j]
-            else:
-                # we are jumping into the function and pulling it back k:
-                flow_i = time_series_list[i][k:]
-                flow_j = time_series_list[j][:-k]
-            # L^1 difference
-            l1_flow_i = np.sum(np.abs(flow_i-flow_j))
-            l1_scores_i.append(l1_flow_i)
-
-        # Combine the L^1 product scores
-        l1_scores = l1_scores_i
-        idx = np.argmin(l1_scores)
-        if i < j:
-            offsets_list.append(idx)
-        else:
-            print("i>j")
-        # L^1 distance
-        l1_matrix[i,j] = np.sum(np.abs(time_series_list[i]-time_series_list[j]))
-    print("Iteration ", i)
-
-# Plot heatmap
-plt.matshow(l1_matrix)
-plt.title("L^1 matrix")
-plt.savefig("L1_time_matrix")
+# Dynamic Time Warping in Time domain
+plt.matshow(dtw_matrix)
 plt.show()
 
-# Compute Mean and diagonal
-print("Mean offset matrix", np.mean(offset_matrix))
-print("Diagonal offset matrix", np.diag(offset_matrix))
-
-# Plot histogram of offsets
-plt.hist(np.array(offset_matrix).flatten(), bins=100)
-plt.xlabel("Offsets")
-plt.ylabel("Frequency")
-plt.show()
+# # Time series i and j
+# for i in range(len(time_series_list)):
+#     for j in range(len(time_series_list)):
+#
+#         # L^1 distance
+#         l1_matrix[i, j] = np.sum(np.abs(time_series_list[i] - time_series_list[j]))
+#
+#         if i <= j:
+#             # l1 lag
+#             l1_scores_i = []
+#             for k in range(0,max_offset,1):
+#                 # if k is equal to 0 (only for no offset)
+#                 if k == 0:
+#                     flow_i = time_series_list[i]
+#                     flow_j = time_series_list[j]
+#                     # L^1 difference
+#                     l1_flow_i = np.sum(np.abs(flow_i - flow_j)) * 1/(len(flow_i)-k)
+#                     l1_scores_i.append(l1_flow_i)
+#
+#                 else:
+#                     # we are jumping into the function and pulling it back k:
+#                     flow_i = time_series_list[i][k:]
+#                     flow_j = time_series_list[j][:-k]
+#                     # L^1 difference
+#                     l1_flow_i = np.sum(np.abs(flow_i-flow_j)) * 1/(len(flow_i)-k)
+#                     l1_scores_i.append(l1_flow_i)
+#
+#             # Append the offset only when we are below the diagonal
+#             idx = np.argmin(l1_scores_i)
+#             offset_matrix[i,j] = idx
+#         else:
+#             offset_matrix[i, j] = 0
+#
+#     # Iteration
+#     print("Iteration ", i)
+#
+# # Plot heatmap
+# plt.matshow(l1_matrix)
+# plt.title("L^1 matrix")
+# plt.savefig("L1_time_matrix")
+# plt.show()
+#
+# # Plot offsets matrix
+# plt.matshow(offset_matrix)
+# plt.title("Offsets matrix")
+# plt.savefig("Offsets_time_matrix")
+# plt.show()
+#
+# # Compute Mean and diagonal
+# print("Mean offset matrix", np.mean(offset_matrix))
+# print("Diagonal offset matrix", np.diag(offset_matrix))
+#
+# # Plot histogram of offsets
+# plt.hist(np.array(offset_matrix).flatten(), bins=100)
+# plt.xlabel("Offsets")
+# plt.ylabel("Frequency")
+# plt.show()
 
 # Plot dendrograms
-dendrogram_plot_labels(offset_matrix, "_time_offsets_", "_L1_", labels=labels)
+# dendrogram_plot_labels(l1_matrix, "_time_offsets_", "_L1_", labels=labels)
+dendrogram_plot_labels(dtw_matrix, "_DTW_offsets_", "_L1_", labels=labels)
